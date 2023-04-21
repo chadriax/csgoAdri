2b3f32d0d6c1097eb09f330acde59d2b240c8c9b
chadriax <i14adriacalvet@gmail.com>
2023-04-21 12:04:24 +0200

    Creation of the project

---
+++ src/test/java/com/csgo/CsgoProjectAdriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgoProjectAdriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
